d3708f65b0cca2de68f1cf7169124a667010b077
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-04-02 16:39:12 -0600

    add block documentation to collection functions

--- src/VideoGameCollection.java
+++ src/VideoGameCollection.java
@@ -31,14 +31,41 @@ public class VideoGameCollection
     /**************************************************************
     * CONSTRUCTORS                                                *
     **************************************************************/
-    // all necessary parameters for attributes
+    
+    /***********************************************************************
+	 * Method:   Constructor (all parameters)                               *
+	 * Purpose:  Create a Video Game based on user parameters               *
+	 * Parameters:                                                          *
+	 *    name_ -- the name of the game collection, as a String             *
+	 *    games_ -- the list of games to use in the collection              *
+	 * Return value: None                                                   *
+	 ***********************************************************************/
+    public VideoGameCollection(String name_, ArrayList<VideoGame> games_)
+    {
+    	this.name = name_;
+    	this.games = games_;
+    }
+    
+    /***********************************************************************
+	 * Method:   Constructor (games omitted)                                *
+	 * Purpose:  Create a Video Game Collection based on user parameters.   *
+	 *           An empty list of games is created by default.              *
+	 * Parameters:                                                          *
+	 *    name_ -- the name of the game collection, as a String             *
+	 * Return value: None                                                   *
+	 ***********************************************************************/
     public VideoGameCollection(String name_)
     {
         this.name = name_;
         this.games = new ArrayList<VideoGame>();
     }
     
-    // no initial parameters
+    /***********************************************************************
+	 * Method:   Constructor (no parameters)                                *
+	 * Purpose:  Creates a blank object.                                    *
+	 * Parameters: None                                                     *
+	 * Return value: None                                                   *
+	 ***********************************************************************/
     public VideoGameCollection()
     {
         this("My video game collection.");
@@ -48,13 +75,48 @@ public class VideoGameCollection
     * GET and SET METHODS                                         *
     **************************************************************/
     
-    // return a comic given the index
+    /***********************************************************************
+	 * Method:  setName                                                     *
+	 * Purpose:  manually sets the title of the game collection             *
+	 * Parameters:                                                          *
+	 *    name_ -- the new title for the game collection, as a String       *
+	 * Return value: None                                                   *
+	 ***********************************************************************/
+    public void setName(String name_)
+    {
+    	this.name = name_;
+    }
+    
+    /***********************************************************************
+	 * Method:  getName                                                     *
+	 * Purpose:  retrieves the title of the game collection                 *
+	 * Parameters: None                                                     *
+	 * Return value: The title of the game collection as a String.          *
+	 ***********************************************************************/
+    public String getName()
+    {
+    	return this.name;
+    }
+    
+    /***********************************************************************
+	 * Method:  getGame                                                     *
+	 * Purpose:  retrieves a specific game in the collection based on an    *
+	 * 		     index provided by the user.                                *
+	 * Parameters:                                                          *
+	 *    index -- specifies what game to retrieve from the collection      *
+	 * Return value: The game at the specified index, as a VideoGame object *
+	 ***********************************************************************/
     public VideoGame getGame(int index)  
     {
         return this.games.get(index);
     }
     
-    // return the number of comics in the array
+    /***********************************************************************
+	 * Method:  getSize                                                     *
+	 * Purpose:  retrieves the current size of the game collection          *
+	 * Parameters: None                                                     *
+	 * Return value: The size of the game collection as an int.             *
+	 ***********************************************************************/
     public int getSize()
     {
         return this.games.size();
@@ -64,7 +126,14 @@ public class VideoGameCollection
     /**************************************************************
     * OUTPUT METHODS                                              *
     **************************************************************/
-    // print the entire collection, with title and summary information
+    
+    /***********************************************************************
+	 * Method:  printCollection	                                            *
+	 * Purpose: Displays name of the game collection, and the games in      *
+	 * 			said collection in an easy to read manner.    				*
+	 * Parameters: none                                                     *
+	 * Return value: None                                                   *
+	 ***********************************************************************/
     public void printCollection()
     {
         System.out.println("\nCollection: "+this.name+"\n");
@@ -77,7 +146,12 @@ public class VideoGameCollection
         System.out.println("--------------------------------------------");
     }
     
-    // print a summary of the collection
+    /***********************************************************************
+	 * Method:  printSummary	                                            *
+	 * Purpose: Displays the descriptions of each game in the collection.   *
+	 * Parameters: none                                                     *
+	 * Return value: None                                                   *
+	 ***********************************************************************/
     public void printSummary()
     {
         for (int i = 0; i < this.games.size(); i++)
