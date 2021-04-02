8d7010651a51c4fb3d41cc19cdf52a89ae603c18
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-04-02 12:41:40 -0600

    remove old collection tester code

--- src/VideoGameCollectionTester.java
+++ src/VideoGameCollectionTester.java
@@ -17,33 +17,33 @@
 *                                                                     *
 **********************************************************************/
 
-public class ComicCollectionTester 
+public class VideoGameCollectionTester 
 {
     
     public static void main(String[] args) 
     {
         // Load initial collection, hardcoded for now
-        ComicCollection collection1 = new ComicCollection("Dean's Comics");
-        ComicBook tempComic;
-        tempComic = new ComicBook("Captain Marvel", 
-                                  1, 
-                                  "Kelly Sue DeConnick", 
-                                  "Scott Hepburn");
-        collection1.addComic(tempComic);
-        
-        tempComic = new ComicBook("Spider-Man", 
-                                  1, 
-                                  "Brian Michael Bendis", 
-                                  "Mark Bagley", 
-                                  "Mint", 
-                                  25.00);
-        collection1.addComic(tempComic);
-        collection1.printCollection();
-        
-        // Make changes through get and set methods
-        collection1.getComic(0).setTitle("Superbwoman");
-        collection1.getComic(1).setArtist("Gary Frank");
-        collection1.printCollection();
+//        ComicCollection collection1 = new ComicCollection("Dean's Comics");
+//        ComicBook tempComic;
+//        tempComic = new ComicBook("Captain Marvel", 
+//                                  1, 
+//                                  "Kelly Sue DeConnick", 
+//                                  "Scott Hepburn");
+//        collection1.addComic(tempComic);
+//        
+//        tempComic = new ComicBook("Spider-Man", 
+//                                  1, 
+//                                  "Brian Michael Bendis", 
+//                                  "Mark Bagley", 
+//                                  "Mint", 
+//                                  25.00);
+//        collection1.addComic(tempComic);
+//        collection1.printCollection();
+//        
+//        // Make changes through get and set methods
+//        collection1.getComic(0).setTitle("Superbwoman");
+//        collection1.getComic(1).setArtist("Gary Frank");
+//        collection1.printCollection();
         
     }
 }
