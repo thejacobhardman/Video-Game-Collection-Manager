0e2c703251ecd15362e0faad596c05e9a7e36fb3
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-04-02 12:56:24 -0600

    format

--- src/VideoGameTester.java
+++ src/VideoGameTester.java
@@ -21,6 +21,43 @@ public class VideoGameTester
 {
     public static void main(String[] args) 
     {
-        
+//        // Load initial collection (hardcoded for now)
+//        ComicBook book1 = new ComicBook("Captain Marvel", 
+//                                        1, 
+//                                        "Kelly Sue DeConnick", 
+//                                        "Scott Hepburn");
+//        ComicBook book2 = book1.createNextInSeries();
+//        ComicBook book3 = new ComicBook("Darth Vader", 
+//                                        1,
+//                                        "Kieron Gillen", 
+//                                        "Salvador Larroca", 
+//                                        "Mint", 
+//                                        3.50);
+//        
+//        // Summary info
+//        System.out.println("My comic books:");
+//        System.out.println("  "+book1.description());
+//        System.out.println("  "+book2.description());
+//        System.out.println("  "+book3.description());
+//        System.out.println("--------------------------------------");
+//        
+//        // Full info
+//        System.out.println("\nDetailed information:\n");
+//        System.out.println(book1.toString());
+//        System.out.println(book2.toString());
+//        System.out.println(book3.toString());
+//        
+//        // Change info
+//        book1.setWriter("Warren Ellis");
+//        book2.setArtist("Jim Lee");
+//        book2.setValue(40);
+//        book3.setCondition("Mint");
+//        book3.setValue(book2.getValue());
+//        
+//        // Full info, reflecting changes
+//        System.out.println("\nDetailed information, with changes:\n");
+//        System.out.println(book1.toString());
+//        System.out.println(book2.toString());
+//        System.out.println(book3.toString());
     }
 }
