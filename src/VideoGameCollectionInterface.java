e7d26631822440fc61692e52913c3df1f0074454
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-04-08 13:14:04 -0600

    add base methods to interface

--- src/VideoGameCollectionInterface.java
+++ src/VideoGameCollectionInterface.java
@@ -34,5 +34,79 @@ public class VideoGameCollectionInterface {
 	private ArrayList<String> validSelections;    
 	private String selection;    
 	private Scanner input;
-
+	private VideoGameCollection collection;
+	
+	public VideoGameCollectionInterface() {
+		this.input = new Scanner(System.in);
+		this.validSelections = new ArrayList<String>();
+		validSelections.add("A");
+		validSelections.add("D");
+		validSelections.add("P");
+		validSelections.add("S");
+		validSelections.add("Q");
+		this.selection = "x";
+		this.collection = new VideoGameCollection("My Favorite Games");
+	}
+	
+	public void DisplayMenu() {
+		System.out.println("Welcome To The VideoGameCollection Interface Program!");
+		System.out.println("You are currently managing the collection: " + this.collection.getName() + ".");
+		System.out.println("Please make a valid selection from the list of options below: ");
+		System.out.println("\n-----------------------------------------\n");
+		System.out.println("'A' - Add new game to collection.");
+		System.out.println("'D' - Delete a game from the collection.");
+		System.out.println("'P' - View a table of all games in the collection.");
+		System.out.println("'S' - Search the collection for a specific game.");
+		System.out.println("'Q' - Quit the program.");
+	}
+	
+	public void AcceptUserInput() {
+		boolean validInput = false;
+		while (!validInput) {
+			System.out.print("Selection: ");
+			String userInput = this.input.nextLine();
+			
+			if (this.validSelections.contains(userInput.toUpperCase())) {
+				this.selection = userInput;
+				validInput = true;
+			} else {
+				System.out.println("Please enter a valid selection.");
+			}
+		}
+	}
+	
+	public void ProcessUserInput() {
+		switch (this.selection) {
+		case "A":
+			//add a game method
+			break;
+		case "D":
+			//delete a game
+			break;
+		case "P":
+			//print table of games
+			break;
+		case "S":
+			//search for a game
+			break;
+		case "Q":
+			//quit game
+			break;
+		default:
+			System.out.println("*FATAL ERROR WHILE PROCESSING USER INPUT*");
+			break;
+		}
+	}
+	
+	public void RunUntilClose() {
+		this.selection = "x";
+		while (!this.selection.equals("Q")) {
+			this.DisplayMenu();
+			this.AcceptUserInput();
+			this.ProcessUserInput();
+		}
+		System.out.println("\nThank you for using the program.");
+		System.out.println("\nExiting now...");
+	}
+	
 }
