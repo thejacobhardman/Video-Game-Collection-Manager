d21632b8b9638f3adbafe86ba4813492004ccdfd
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-04-08 12:48:47 -0600

    create new project files

---
+++ src/VideoGameCollectionInterface.java
@@ -0,0 +1,34 @@
+/**********************************************************************
+*                   Assignment 8 -- Video Game Collection Interface   *
+*                                                                     *
+* PROGRAMMER:        Jacob Hardman - hard7293@bears.unco.edu          *
+* CLASS:             CS200 – Object Oriented Programming              *
+* INSTRUCTOR:        Dean Zeller                                      *
+* TERM:              Spring 2021                                      *
+* SUBMISSION DATE:   4/9/2021		                                  *
+*                                                                     *
+* DESCRIPTION:                                                        *
+* The following is an OOP definition for a video game collection.     *
+*                                                                     *
+* ATTRIBUTES:                                                         *
+* 	name -- The name of the collection, stored as a String.           *
+* 	games -- An ArrayList that stores the individual VideoGame        *
+* 			 objects.												  *
+*                                                                     *
+* COPYRIGHT:                                                          *
+* This program is copyright (c)2020 Jacob Hardman and Dean Zeller.    *
+* Based on a template written by Tom Jensen and Dean Zeller.          *
+* It is original work without use of outside sources.                 *
+*                                                                     *
+**********************************************************************/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class VideoGameCollectionInterface {
+	
+	private ArrayList<String> validSelections;    
+	private String selection;    
+	private Scanner input;
+
+}
---
+++ src/VideoGameCollectionInterfaceTester.java
@@ -0,0 +1,32 @@
+/**********************************************************************
+*                   Assignment 8 -- Video Game Collection Interface   *
+*                                                                     *
+* PROGRAMMER:        Jacob Hardman - hard7293@bears.unco.edu          *
+* CLASS:             CS200 – Object Oriented Programming              *
+* INSTRUCTOR:        Dean Zeller                                      *
+* TERM:              Spring 2021                                      *
+* SUBMISSION DATE:   4/9/2021		                                  *
+*                                                                     *
+* DESCRIPTION:                                                        *
+* The following is an OOP definition for a video game collection.     *
+*                                                                     *
+* ATTRIBUTES:                                                         *
+* 	name -- The name of the collection, stored as a String.           *
+* 	games -- An ArrayList that stores the individual VideoGame        *
+* 			 objects.												  *
+*                                                                     *
+* COPYRIGHT:                                                          *
+* This program is copyright (c)2020 Jacob Hardman and Dean Zeller.    *
+* Based on a template written by Tom Jensen and Dean Zeller.          *
+* It is original work without use of outside sources.                 *
+*                                                                     *
+**********************************************************************/
+
+public class VideoGameCollectionInterfaceTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
